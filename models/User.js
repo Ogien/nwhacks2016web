bae8d71a1207110fe697aa110161e55310cbe2fa
Alex Land <aland@sfu.ca>
2016-02-27 18:21:29 -0800

    modify user schema for the recruiter (candidates will not have profiles on the database)

--- models/User.js
+++ models/User.js
@@ -8,22 +8,17 @@ var userSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetExpires: Date,
 
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
   linkedin: String,
-  steam: String,
-  tokens: Array,
 
   profile: {
     name: { type: String, default: '' },
     gender: { type: String, default: '' },
     location: { type: String, default: '' },
-    website: { type: String, default: '' },
     picture: { type: String, default: '' }
-  }
+  },
+
+  candidatesList: String
+
 }, { timestamps: true });
 
 /**
